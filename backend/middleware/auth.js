583346570411de4f846ae653f0f5c0d6d7ddcdcb
rajankumarrkr <rajankumarrkr4576@gmail.com>
2026-05-01 16:53:00 +0530

    Prioritize Authorization header over cookies

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,10 +4,10 @@ import User from '../models/User.js';
 export const protect = async (req, res, next) => {
     let token;
 
-    if (req.cookies && req.cookies.token) {
-        token = req.cookies.token;
-    } else if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies && req.cookies.token) {
+        token = req.cookies.token;
     }
 
     if (!token) {
